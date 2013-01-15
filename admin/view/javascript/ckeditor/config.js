05bc24d856d380fa5a41e9bfd8bd00ea4d7e581d
mib <info@vilkov.info>
2013-01-15 23:56:02 +0200

    обновлен ckeditor до версии 4

--- admin/view/javascript/ckeditor/config.js
+++ admin/view/javascript/ckeditor/config.js
@@ -1,62 +1,47 @@
-﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
+	// config.language = 'en';
 	// config.uiColor = '#AADC6E';
-	/*
+	
 	config.filebrowserBrowseUrl = 'index.php?route=common/filemanager';
 	config.filebrowserImageBrowseUrl = 'index.php?route=common/filemanager';
 	config.filebrowserFlashBrowseUrl = 'index.php?route=common/filemanager';
 	config.filebrowserUploadUrl = 'index.php?route=common/filemanager';
 	config.filebrowserImageUploadUrl = 'index.php?route=common/filemanager';
-	config.filebrowserFlashUploadUrl = 'index.php?route=common/filemanager';		
-	*/
+	config.filebrowserFlashUploadUrl = 'index.php?route=common/filemanager';
 	
-	config.filebrowserWindowWidth = '800';
-	config.filebrowserWindowHeight = '500';
-
-	config.resize_enabled = false;
+	config.filebrowserWindowWidth = '960';
+	config.filebrowserWindowHeight = '580';
 	
 	config.htmlEncodeOutput = false;
 	config.entities = false;
-	
-	config.toolbar = 'Custom';
 
-	config.toolbar_Custom = [
-		['Source'],
-		['Maximize'],
-		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-		['NumberedList','BulletedList','-','Outdent','Indent'],
-		['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-		['SpecialChar'],
-		'/',
-		['Undo','Redo'],
-		['Font','FontSize'],
-		['TextColor','BGColor'],
-		['Link','Unlink','Anchor'],
-		['Image','Table','HorizontalRule']
-	];
-	
-	config.toolbar_Full = [
-		['Source','-','Save','NewPage','Preview','-','Templates'],
-		['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
-		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-		['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
-		'/',
-		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-		['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-		['Link','Unlink','Anchor'],
-		['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
-		'/',
-		['Styles','Format','Font','FontSize'],
-		['TextColor','BGColor'],
-		['Maximize', 'ShowBlocks','-','About']
-	];
-
+    config.contentsLangDirection = 'ltr';
+	//config.contentsLangDirection = 'rtl';
+	config.skin = 'kama';
+	// config.skin = 'moono';
+	config.toolbar = 'full';
+	// config.toolbar = 'Custom';
+ 
+//config.toolbar_Custom =
+//[
+//   { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+//   { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+//   { name: 'forms', items : [ 'Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField' ] },
+//   { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] },
+//   '/',
+//   { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+//   { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+//   { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+//   { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+//   '/',
+//   { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+//   { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+//   { name: 'colors', items : [ 'TextColor','BGColor' ] }
+//];
 };
