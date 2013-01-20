e7a3bd1f01017413b1960f255171604353e8adbe
mib <info@vilkov.info>
2013-01-20 17:32:45 +0200

    42. Добавлен Модуль Табы 4 в одном Новинки \ Акции \ Рекомендуемые \ Хиты продаж

---
+++ admin/language/ukrainian/module/product_tab.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']       = 'Модуль Табы';
+
+// Text
+$_['text_module']         = 'Модулі';
+$_['text_success']        = 'Налаштування модуля успішно змінено!';
+$_['text_content_top']    = 'Над контентом';
+$_['text_content_bottom'] = 'Під контентом';
+$_['text_column_left']    = 'Ліва колонка';
+$_['text_column_right']   = 'Права колонка';
+
+// Entry
+$_['entry_limit']         = 'Обмеження:';
+$_['entry_image']         = 'Зображення (Ш х В):';
+$_['entry_layout']        = 'Макет:';
+$_['entry_position']      = 'Позиція:';
+$_['entry_status']        = 'Статус:';
+$_['entry_sort_order']    = 'Порядок сортування:';
+
+// Error
+$_['error_permission']    = 'Ви не маєте повноважень для зміни модуля!';
+$_['error_image']         = 'Необхідно вказати розміри зображення!';
+?>
---
+++ catalog/language/ukrainian/module/product_tab.php
@@ -0,0 +1,12 @@
+<?php
+// Heading 
+$_['heading_title']  = 'Товари';
+
+$_['tab_latest']     = 'Новинки';
+$_['tab_bestseller'] = 'Лідери продажу';
+$_['tab_featured']   = 'Рекомендовані';
+$_['tab_special']    = 'Акційні';
+
+// Text
+$_['text_reviews']   = 'Базовано на %s відгуках.'; 
+?>
