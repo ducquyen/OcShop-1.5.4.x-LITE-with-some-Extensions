83a1c2f4f3e9605fdc28548e28adb7fbbfdd3a9d
mib <info@vilkov.info>
2013-01-20 17:06:12 +0200

    41. установлен Модуль HTML добавлено условие отображения стиля блока когда нет заголовка

---
+++ admin/language/ukrainian/module/blockhtml.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']       	= 'Модуль HTML'; 
+
+// Text
+$_['text_module']         	= 'Модулі';
+$_['text_success']        	= 'Налаштування модуля успішно змінено!';
+$_['text_content_top']      = 'Над контентом';
+$_['text_content_bottom']   = 'Під контентом';
+$_['text_column_left']      = 'Ліва колонка';
+$_['text_column_right']     = 'Права колонка';
+
+// Entry
+$_['entry_html'] 			= 'Текст HTML';
+$_['entry_yes'] 			= 'Да';
+$_['entry_no'] 				= 'Ні';
+$_['entry_title'] 			= 'Назва <p class="help">Текст заголовка</p>';
+$_['entry_header'] 		    = 'Заголовок <p class="help">Відображати заголовок</p>';
+$_['entry_layout']          = 'Макет:';
+$_['entry_position']        = 'Позиція:';
+$_['entry_status']          = 'Статус:';
+$_['entry_sort_order']      = 'Порядок сортування:';
+
+// Error
+$_['error_permission']      = 'Ви не маєте повноважень для зміни модуля!';
+?>
