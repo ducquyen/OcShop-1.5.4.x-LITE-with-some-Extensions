49cd6070255a94c3ec789f9b6d9f41aee2e0b11e
mib <info@vilkov.info>
2013-01-21 15:54:41 +0200

    50. Добавлен способ доставки Новая Почта

---
+++ admin/language/english/shipping/novaposhta.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']    = 'NovaPoshta';
+
+// Text 
+$_['text_shipping']    = 'Shipping';
+$_['text_success']     = 'Success: You have modified NovaPoshta shipping!';
+
+// Entry
+$_['entry_geo_zone']   = 'Geo Zone:';
+$_['entry_status']     = 'Status:';
+$_['entry_sort_order'] = 'Sort Order:';
+
+$_['entry_delivery_price'] = 'The cost of shipping fee:<br /><span class="help">If the order is less than the minimum amount for free shipping.</span>';
+$_['entry_min_total_for_free_delivery'] = 'The minimum order for free delivery:<br /><span class="help">If the order is greater than or equal to the value, then the cost of delivery to the customer will be zero.</span>';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify NovaPoshta shipping!';
+?>
---
+++ catalog/language/english/shipping/novaposhta.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_title']       = 'NovaPoshta';
+$_['text_description'] = 'NovaPoshta.';
+?>
