d1287081709b987ec26eb7d727734a1807c1a37f
mib <info@vilkov.info>
2013-01-21 15:51:47 +0200

    49. Добавлен способ оплаты Наложенный платеж

---
+++ catalog/language/english/payment/nalogenniy_np.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']       = 'Cash on Delivery';
+$_['text_instruction'] = 'Cash on Delivery';
+$_['text_payment']     = 'To clarify the order, you can contact us by phone.';
+?>
